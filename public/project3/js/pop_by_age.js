500e09e849e85ba5fcb571bff8eab83cddd0ac4a
lilartus <lilit.mardanyan@tumo.org>
2017-12-29 17:27:30 +0400

    creating actors 3d graphs

--- public/project3/js/pop_by_age.js
+++ public/project3/js/pop_by_age.js
@@ -5,8 +5,9 @@ $.getJSON("data/populations.json", function(jsonData){
   // populate colData
 
   for(var i = 0; i < jsonData.USA.length; i++){
- 	var psh = [jsonData.USA[i].age[0] + jsonData.USA[i].age[1] + "-" + jsonData.USA[i].age[2] + jsonData.USA[i].age[3], jsonData.USA[i].value]
+ 	var psh = [jsonData.USA[i].age[0] + jsonData.USA[i].age[1] + "-" + jsonData.USA[i].age[2] + jsonData.USA[i].age[3], jsonData.USA[i].value];
   	colData.push(psh);
+
   }
   console.log(jsonData)
 	chart = c3.generate({
